882fd6b7b010e12825f5bd39feb8b47d04de446b
Subhra S. Sarkar <rurtle.coder@gmail.com>
2026-02-03 23:17:18 +0000

    Modified the finder script, removed extra comments

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Accepts the following runtime arguments: the first argument is a path to a directory on the filesystem, referred to below as filesdir; the second argument is a text string which will be searched within these files, referred to below as searchstr
-# Exits with return value 1 error and print statements if any of the parameters above were not specified
-# Exits with return value 1 error and print statements if filesdir does not represent a directory on the filesystem
-# Prints a message "The number of files are X and the number of matching lines are Y" where X is the number of files in the directory and all subdirectories and Y is the number of matching lines found in respective files, where a matching line refers to a line which contains searchstr (and may also contain additional content).
-
 ## Making sure the script accepts only 2 arguments
 if [ $# -ne 2 ]; then
     echo "Error: Two arguments are required."
